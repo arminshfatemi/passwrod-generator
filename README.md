7e675888bf56a0bf613a7a928699a4632cfa5313
Armin <arminsh610@gmail.com>
2023-12-28 14:32:49 +0330

    add README.md and .gitignore

--- README.md
+++ README.md
@@ -1,23 +1,69 @@
-# passwrod generator
-  this is a password generator coded with python , and tkinter library 
+# Password generator
+A Password generator built with python tkinter
 
- i put 4 types of password :
+### Features
+1. Copy generated password to clipboard with a button
+2. You can choose the length of your password 
+3. You can choose the level of generated password
+4. An Error Entry to show the error 
 
- 1- weak : its just lowercase 
+
+### Password Levels 
+ 1. weak : its just lowercase Characters
  
- 2-medium : its numbers and lowecase 
+ 2. medium : its numbers and lowecase Characters
  
- 3-strong : lowercase , uppercase,numbers
+ 3. strong : lowercase ,uppercase ,numbers Characters
  
- 4-Excellent : lowercase ,uppercase,numbers, and symbols
-
- you can choose how many charachers do your password should have in length Entry
-
- and i put a copy button to copy generated password to your clipboard 
-
- and a Error Entry to say whats wrong 
-
- thanks.
+ 4. Excellent : lowercase ,uppercase ,numbers and symbols Characters
 
 
+
+### How to Run
+
+1. First clone the project and cd to project 
+```commandline
+git clone <url of the project>
+cd Password-generator
+```
+2. Make a python virtual env
+```commandline
+python3 -m venv <name of your venv>
+```
+3. Activate your venv
+#### on linux
+```commandline
+source ./venv/bin/activate
+```
+#### on windows
+```commandline
+venv/bin/activate
+```
+
+4. Install the python packages
+```commandline
+pip install -r requirements.txt
+```
+5. Run the file
+```commandline
+python password-generator.py
+```
+
+### Error
+if you have error like this :
+
+ERROR: Could not find a version that satisfies the requirement tkinter (from versions: none)
+
+ERROR: No matching distribution found for tkinter
+
+first make sure you have installed packages and venv is activated 
+if it didn't fixed do below command 
+
+```commandline
+ sudo dnf install python3.<your version>-tkinter  
+```
+
+and i hope it fixed 
+
+Enjoy.
 
---
+++ password-generator.py
@@ -0,0 +1,136 @@
+import string
+from tkinter import *
+import random
+import pyperclip  as pyco
+
+root = Tk()
+root.title("Password Generator")
+root.geometry("750x350")
+root.resizable(True, True)
+
+
+
+upper_cases = string.ascii_uppercase
+lower_cases = string.ascii_lowercase
+var_numbers = string.digits
+symbols = string.punctuation
+
+
+
+def type_pass():
+
+    if check_value.get() == 0:
+        entry_error.delete(0,END)
+        entry_error.insert(0, "Please choose atleast one type of password")
+
+    if check_value.get() == 1:
+        current_password = str(lower_cases)
+
+        return current_password
+
+
+
+    elif check_value.get() == 2 :
+        current_password = str(lower_cases) + str(var_numbers)
+
+        return current_password
+
+
+
+    elif check_value.get() == 3 :
+        current_password = str(lower_cases) + str(var_numbers) + str(upper_cases)
+
+        return current_password
+
+
+    elif check_value.get() == 4 :
+        current_password = str(lower_cases) + str(var_numbers) + str(upper_cases) + str(symbols)
+
+        return current_password
+
+
+
+
+
+
+
+
+
+def clipboard_copy():
+    pass_copy = show_entry.get()
+    pyco.copy(pass_copy)
+
+
+
+def generate():
+    try:
+        len_k = int(length_entry.get())
+    except:
+        entry_error.delete(0,END)
+        entry_error.insert(0,"Please write a the length !!")
+    if len_k == 0 :
+        entry_error.delete(0,END)
+        entry_error.insert(0,"Length cant be 0 !!")
+
+
+
+    pass_random = random.choices(type_pass(),k=len_k)
+    show_entry.delete(0, END)
+    for x in pass_random:
+        show_entry.insert(10,x)
+
+
+
+
+
+
+
+
+
+
+frame = LabelFrame(root, text="Write down the LENGTH of password.", labelanchor="n", )
+frame.grid(pady=30,padx=30, row=0,column=0)
+
+
+
+length_entry = Entry(frame, width=30, bd=5,)
+length_entry.grid(pady=20, padx=20)
+
+
+check_value = IntVar()
+
+frame_checkbox = LabelFrame(root,text="choose type of password.",labelanchor="n")
+frame_checkbox.grid(padx=20,pady=10,row=0,column=1,columnspan=2)
+
+checkbox_1 = Radiobutton(frame_checkbox,text="Weak", value=1, variable=check_value,)
+checkbox_2 = Radiobutton(frame_checkbox,text="Medium", value=2, variable=check_value,)
+checkbox_3 = Radiobutton(frame_checkbox,text="Strong", value=3, variable=check_value,)
+checkbox_4 = Radiobutton(frame_checkbox,text="Excellent", value=4, variable=check_value,)
+
+checkbox_1.grid(padx=10,row=0,column=0)
+checkbox_2.grid(padx=10,row=0,column=1)
+checkbox_3.grid(padx=10,row=0,column=2)
+checkbox_4.grid(padx=10,row=0,column=3)
+
+button_copy = Button(root,text="copy",padx=20,command=clipboard_copy)
+button_generate = Button(root,text="Generate",padx=20,command=generate)
+button_copy.grid(row=1,column=1)
+button_generate.grid(row=1,column=2)
+
+
+frame_entry = LabelFrame(root,text="Generated Password",labelanchor="n",)
+
+show_entry = Entry(frame_entry,width=30,bd=5)
+
+frame_entry.grid(row=1,column=0)
+show_entry.grid(padx=20,pady=20)
+
+
+
+frame_status = LabelFrame(root,text="errors")
+frame_status.grid(row=2,column=0,padx=30,pady=20)
+
+entry_error = Entry(frame_status,width=40,bd=5)
+entry_error.pack(pady=20,padx=20)
+
+root.mainloop()
